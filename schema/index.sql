67482e350f75dad69199b015932e4d2ebbddd777
samshal <samueladeshina73@gmail.com>
2021-07-26 20:26:05 +0100

    Update db

--- schema/index.sql
+++ schema/index.sql
@@ -5,8 +5,7 @@ CREATE TABLE users (
 	user_password VARCHAR(500) NOT NULL,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50)
-)
-GO;
+);
 
 CREATE TABLE bvn_retrieved_bvns (
 	bvn_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -15,8 +14,7 @@ CREATE TABLE bvn_retrieved_bvns (
 	retrieved_by INT,
 	CONSTRAINT fk_retrievedBvns_retrievedBy_users_userId
 		FOREIGN KEY (retrieved_by) REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE SET NULL
-)
-GO;
+);
 
 CREATE TABLE bvn_retrieved_bvn_data (
 	bvn_data_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -25,8 +23,7 @@ CREATE TABLE bvn_retrieved_bvn_data (
 	data_value VARCHAR(MAX),
 	CONSTRAINT fk_retrievedBvnData_bvn_retrievedBvns_bvn
 		FOREIGN KEY (bvn) REFERENCES bvn_retrieved_bvns (bvn) ON UPDATE CASCADE ON DELETE CASCADE
-)
-GO;
+);
 
 CREATE TABLE logs_bvn_search (
 	search_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -35,5 +32,4 @@ CREATE TABLE logs_bvn_search (
 	search_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT fk_bvnSearch_userId_users_userId
 		FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE SET NULL
-)
-GO;
+);
