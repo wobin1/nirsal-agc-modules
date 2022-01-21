f9ec637478402b8ed53338aea30a9361e1c9cda4
Nathaniel Dauda Wobin <42215652+wobin1@users.noreply.github.com>
2022-01-21 09:55:44 +0100

    minor commit

--- src/Agc/Application.php
+++ src/Agc/Application.php
@@ -196,7 +196,7 @@ class Application
 		$farmer_crc_status = $data["crcStatus"];
 		
 
-		$query = "INSERT INTO agc_application_kyc_farmers_data (application_id, farmers_name, farmer_bvn, farmer_phone, crc_status) VALUES ('$application_id, $famer_name', '$bvn', '$farmer_phone', '$farmer_crc_status')";
+		$query = "INSERT INTO agc_application_kyc_farmers_data (application_id, farmer_bvn, farmers_name, farmer_phone, crc_status) VALUES ('$application_id, '$bvn', $famer_name', '$farmer_phone', '$farmer_crc_status')";
 
 		$result = DBConnectionFactory::getConnection()->exec($query);
 
