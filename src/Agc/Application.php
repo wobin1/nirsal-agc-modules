dc64795f8ab23647d130802781ae92258922f5c9
Nathaniel Dauda Wobin <42215652+wobin1@users.noreply.github.com>
2022-02-07 10:27:21 +0100

    minor edits

--- src/Agc/Application.php
+++ src/Agc/Application.php
@@ -243,13 +243,7 @@ class Application
 		return $result;
 	}
 
-	public statuc function getAgcWithCompletedFieldverification(){
-		$query = "SELECT * FROM agc_application_kyc_data WHERE field_verification_status = 1";
-		$result = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
-
-		return $result;
-
-	}
+	
 
 	public static function getAgcFarmers(int $application_id){
 		$query = "SELECT * FROM  agc_application_kyc_farmers_data WHERE application_id= $agcId";
